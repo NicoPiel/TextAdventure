557443798b37aac5baef5459022771b2325ac3aa
Nico Piel <nico.piel@hotmail.de>
2019-12-20 03:02:30 +0100

    Enemy health stuff

--- src/main/java/de/nicrizzos/game/entities/Enemy.java
+++ src/main/java/de/nicrizzos/game/entities/Enemy.java
@@ -2,18 +2,28 @@ package de.nicrizzos.game.entities;
 
 public class Enemy extends Entity {
       private int health;
+      private int currentHealth;
       private int damage;
       private int defense;
       
       public Enemy(String _name, int _health, int _damage, int _defense) {
             super(_name, "enemy_" + _name);
             setHealth(_health);
+            setCurrentHealth(getHealth());
             setDamage(_damage);
             setDefense(_defense);
       }
       
       public int getHealth() {
             return this.health;
+      }
+      
+      public int getCurrentHealth() {
+            return currentHealth;
+      }
+      
+      public double getHealthPercentage () {
+            return (double) getCurrentHealth()/getHealth();
       }
       
       public int getDamage() {
@@ -28,6 +38,10 @@ public class Enemy extends Entity {
             this.health = _health;
       }
       
+      public void setCurrentHealth(int currentHealth) {
+            this.currentHealth = currentHealth;
+      }
+      
       public void setDamage(int _damage) {
             this.damage = _damage;
       }
@@ -35,4 +49,8 @@ public class Enemy extends Entity {
       public void setDefense(int _defense) {
             this.defense = _defense;
       }
+      
+      
+      
+      
 }
